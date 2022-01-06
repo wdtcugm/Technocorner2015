f926ca59ac1b66e92076f8f0c0df8752be209aaa
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2022-01-06 09:47:26 +0700

    add composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c968672d46401ef5cbc3c16135ca00501fce20f4',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c968672d46401ef5cbc3c16135ca00501fce20f4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
